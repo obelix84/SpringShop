46eb577972a2557387f7075aa5669b27971e5bcd
obelix <sonicx921@yandex.ru>
2021-12-12 17:03:01 +0300

    lesson4

---
+++ lesson4_2/src/main/java/ru/gb/application/Lesson4App.java
@@ -0,0 +1,11 @@
+package ru.gb.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lesson4App {
+    public static void main(String[] args) {
+        SpringApplication.run(Lesson4App.class);
+    }
+}
